9be07b46d1570f3bdb7d460c78d9eb65c633d93f
xiaomei <testcomt/users.noreply.github.com>
2016-08-18 21:37:26 +0800

    completion of calculation program initially, with ugly looking GUI

--- src/com/example/firstidea/AppSwing.java
+++ src/com/example/firstidea/AppSwing.java
@@ -10,12 +10,61 @@ import java.awt.event.ActionListener;
 public class AppSwing {
     public JButton ButtonItem;
     public JPanel panelMain;
+    private JLabel lblA;
+    private JLabel lblOp;
+    private JLabel lblB;
+    private JTextField textAnswer;
+    private JButton buttonJudge;
+    private  double result=0.0;
 
     public AppSwing() {
+
+
+
         ButtonItem.addActionListener(new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"Hello World!");
+
+                int a = (int) (Math.random()*9+1);
+                int b = (int) (Math.random()*9+1);
+                int sign = (int) (Math.random()*4+1);
+                String op="";
+
+                switch (sign)
+                {
+                    case 1:
+                        op="+";
+                        result =a+b;break;
+                    case 2:
+                        op="-";
+                        result =a-b;break;
+                    case 3:
+                        op="*";
+                        result =a*b;break;
+                    case 4:
+                        op="/";
+                        result =a/b;break;
+                }
+
+                lblA.setText(""+a);
+                lblB.setText(""+b);
+                lblOp.setText(""+op);
+                textAnswer.setText("");
+            }
+        });
+
+
+        buttonJudge.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String str = textAnswer.getText();
+                double inputResult = Double.parseDouble(str);
+                if (result==inputResult)
+                    JOptionPane.showMessageDialog(null,"Your answer is right!");
+                else
+                    JOptionPane.showMessageDialog(null,"Sorry, wrong answer!");
+
             }
         });
     }
