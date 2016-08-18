c492a9e9b0b747749395555bf50390cab05d8cc9
xiaomei <testcomt/users.noreply.github.com>
2016-08-18 10:21:23 +0800

    initial commit with a hello class

---
+++ src/com/example/firstidea/FirstIdea.java
@@ -0,0 +1,11 @@
+package com.example.firstidea;
+
+/**
+ * Created by taixiaomei on 7/23/16.
+ */
+public class FirstIdea {
+    public static void main(String[] args) {
+        System.out.println("FirstIdea.main");
+        System.out.println("HelloWorld");
+    }
+}
